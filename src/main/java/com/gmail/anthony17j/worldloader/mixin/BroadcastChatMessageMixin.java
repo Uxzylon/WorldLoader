8fde05cf7d3ac29dab785f8aa1c247a95b92d064
Uxzylon <anthony17j@gmail.com>
2022-07-04 22:10:56 +0200

    1.19 support

--- src/main/java/com/gmail/anthony17j/worldloader/mixin/BroadcastChatMessageMixin.java
+++ src/main/java/com/gmail/anthony17j/worldloader/mixin/BroadcastChatMessageMixin.java
@@ -1,26 +1,21 @@
 package com.gmail.anthony17j.worldloader.mixin;
 
-import com.gmail.anthony17j.worldloader.WorldLoader;
-import net.minecraft.network.MessageType;
+import net.minecraft.network.message.MessageType;
 import net.minecraft.server.PlayerManager;
-import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.util.registry.RegistryKey;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.UUID;
-
 @Mixin(PlayerManager.class)
 public class BroadcastChatMessageMixin {
 
     @Inject(method = "broadcast", at = @At("INVOKE"), cancellable = true)
-    void filterBroadCastMessages(Text message, MessageType type, UUID sender, CallbackInfo ci) {
-        String username = ((LiteralText)((TranslatableText) message).getArgs()[0]).getString();
+    void broadcast(Text message, RegistryKey<MessageType> typeKey, CallbackInfo ci) {
 
-        if (username.equals("#worldloader#")) {
+        if (message.getString().contains("#worldloader#")) {
             ci.cancel();
         }
     }
